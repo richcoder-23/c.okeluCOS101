f379eb14daf36b982bbd0db050cd9a7ef6b25380
richcoder-23 <chinemelum.richard@gmail.com>
2023-12-19 10:03:03 +0100

    (week 10)

---
+++ week-10/practice_3/target/debug/deps/practice_3.d
@@ -0,0 +1,5 @@
+C:\Users\chine\Documents\c.okeluCOS101\week-10\practice_3\target\debug\deps\practice_3.exe: src\main.rs
+
+C:\Users\chine\Documents\c.okeluCOS101\week-10\practice_3\target\debug\deps\practice_3.d: src\main.rs
+
+src\main.rs:
---
+++ week-10/practice_7/target/debug/practice_7.d
@@ -0,0 +1,1 @@
+C:\Users\chine\Documents\c.okeluCOS101\week-10\practice_7\target\debug\practice_7.exe: C:\Users\chine\Documents\c.okeluCOS101\week-10\practice_7\src\main.rs
