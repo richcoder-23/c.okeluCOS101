89007eccc5881c95656ae97d8dd5e4746e2e48db
richcoder-23 <chinemelum.richard@gmail.com>
2024-01-26 12:02:54 +0100

    (Week 14)

---
+++ week-14/target/debug/deps/week_14.d
@@ -0,0 +1,5 @@
+C:\Users\chine\Documents\c.okeluCOS101\week-14\target\debug\deps\week_14.exe: src\main.rs
+
+C:\Users\chine\Documents\c.okeluCOS101\week-14\target\debug\deps\week_14.d: src\main.rs
+
+src\main.rs:
---
+++ week-14/target/debug/week-14.d
@@ -0,0 +1,1 @@
+C:\Users\chine\Documents\c.okeluCOS101\week-14\target\debug\week-14.exe: C:\Users\chine\Documents\c.okeluCOS101\week-14\src\main.rs
